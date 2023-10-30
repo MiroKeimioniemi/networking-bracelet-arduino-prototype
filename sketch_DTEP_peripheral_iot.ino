290963a7e5e760c82567ecdafea4b280029ccfe9
Miro Keimiöniemi <65595542+MiroKeimioniemi@users.noreply.github.com>
2023-10-30 17:43:08 +0200

    Add files via upload

---
+++ sketch_DTEP_peripheral_iot.ino
@@ -0,0 +1,60 @@
+#include <ArduinoBLE.h>
+      
+enum {
+  GESTURE_NONE  = -1,
+  GESTURE_UP    = 0,
+  GESTURE_DOWN  = 1,
+  GESTURE_LEFT  = 2,
+  GESTURE_RIGHT = 3
+};
+
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+
+int gesture = -1;
+
+BLEService gestureService(deviceServiceUuid); 
+BLEByteCharacteristic gestureCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);  
+  
+  if (!BLE.begin()) {
+    Serial.println("- Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
+  BLE.setAdvertisedService(gestureService);
+  gestureService.addCharacteristic(gestureCharacteristic);
+  BLE.addService(gestureService);
+  gestureCharacteristic.writeValue(-1);
+  BLE.advertise();
+
+  Serial.println("Nano 33 BLE (Peripheral Device)");
+  Serial.println(" ");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+  Serial.println("- Discovering central device...");
+  delay(500);
+
+  if (central) {
+    Serial.println("* Connected to central device!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(central.address());
+    Serial.println(" ");
+
+    while (central.connected()) {
+      if (gestureCharacteristic.written()) {
+         gesture = gestureCharacteristic.value();
+        
+       }
+    }
+    
+    Serial.println("* Disconnected to central device!");
+  }
+}
