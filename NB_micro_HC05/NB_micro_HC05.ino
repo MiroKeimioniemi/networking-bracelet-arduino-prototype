00089668fdd4731baf6ef96526fc4d5cd727ac7a
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-11-21 00:34:10 +0200

    Made Arduinos continuously exchange their IDs, which gets them to light up LED strips that turn off after 0.1sec of not doing so. Also made IDs configurable via commands of the form @newID.

--- NB_micro_HC05/NB_micro_HC05.ino
+++ NB_micro_HC05/NB_micro_HC05.ino
@@ -1,31 +1,94 @@
-#include<SoftwareSerial.h>
+#include <FastLED.h>
 
-/* Create object named bt of the class SoftwareSerial */ 
-// WORKS WITH UNO
-// SoftwareSerial bt(2,3); /* (Rx,Tx) */ 
+#define NUM_LEDS 9
+#define DATA_PIN 6
 
-void setup(){
-  // WORKS WITH UNO
-  // bt.begin(9600); /* Define baud rate for software serial communication */
-  Serial.begin(9600);  /* Define baud rate for serial communication */
+CRGB leds[NUM_LEDS];
+uint32_t matchIDs[] = {123, 456, 789, 111, 222, 333};
+size_t matchArraySize = sizeof(matchIDs) / sizeof(matchIDs[0]);
+uint32_t ID = 123;
+uint32_t matchID = 0;
+unsigned long lastMatchTime = 0;
+const unsigned long matchTimeout = 100;
+
+void containsValue(const uint32_t array[], size_t size, uint32_t targetValue) {
+  for (size_t i = 0; i < size; i++) {
+    if (array[i] == targetValue) {
+      // Turn on the LEDs
+      leds[0] = CRGB::Red;
+      leds[1] = CRGB::Red;
+      leds[2] = CRGB::Red;
+      leds[3] = CRGB::Red;
+      leds[4] = CRGB::Red;
+      leds[5] = CRGB::Red;
+      leds[6] = CRGB::Red;
+      leds[7] = CRGB::Red;
+      leds[8] = CRGB::Red;
+      return;
+    }
+  }
+}
+
+void setup() {
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  Serial.begin(9600);
   Serial1.begin(9600);
 }
 
-void loop(){
-    if (Serial1.available()) /* If data is available on serial port */
-    {
-      while(Serial1.available()) {
-        Serial.write(Serial1.read());      /* Print character received on to the serial monitor */
+void loop() {
+  char prefix = 0;  // Variable to store the prefix
+  // Broadcast ID
+  char buffer[10];
+  sprintf(buffer, "/%u", ID);
+  Serial1.write(buffer);
+  delay(5);
+
+  if (Serial1.available() >= 1) {
+    char firstChar = Serial1.read();
+
+    // Check for prefix
+    if (firstChar == '/' || firstChar == '@') {
+      prefix = firstChar;
+
+      // Read the next sizeof(uint32_t) bytes into the appropriate variable
+      char buffer[sizeof(uint32_t) * 2 + 1]; // Twice the size for hex digits plus null terminator
+      Serial1.readBytes(buffer, sizeof(uint32_t) * 2);
+      buffer[sizeof(uint32_t) * 2] = '\0'; // Null-terminate the string
+
+      // Convert the string to uint32_t
+      if (prefix == '/') {
+        matchID = strtoul(buffer, NULL, 10); // Use base 10 for decimal representation
+        Serial.print("Received matchID: ");
+        Serial.println(matchID, DEC); // Print matchID in decimal format
+        lastMatchTime = millis();
+      } else if (prefix == '@') {
+        ID = strtoul(buffer, NULL, 10);
+        Serial.print("Received ID: ");
+        Serial.println(ID, DEC); // Print ID in decimal format
       }
     }
+  }
 
-    if (Serial.available()) {
-      Serial1.write(Serial.read());
-    }
+  if (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
 
-    /**if (Serial1.availableForWrite())
-    {
-      Serial1.write("Hello master!\n");
-      delay(1000);
-    }**/
+  Serial.println(millis() - lastMatchTime);
+  containsValue(matchIDs, matchArraySize, matchID);
+
+  // No match found
+  if ((millis() - lastMatchTime) > matchTimeout) {
+    // No recent match, turn off the LEDs
+    leds[0] = CRGB::Black;
+    leds[1] = CRGB::Black;
+    leds[2] = CRGB::Black;
+    leds[3] = CRGB::Black;
+    leds[4] = CRGB::Black;
+    leds[5] = CRGB::Black;
+    leds[6] = CRGB::Black;
+    leds[7] = CRGB::Black;
+    leds[8] = CRGB::Black;
+  }
+
+  FastLED.show();
 }
