b14bdceea23b6f72cecb436fcb7e117dea0b8347
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-11-21 01:56:53 +0200

    Added automatic light pattern generation based on using IDs as seeds for random number generators

--- NB_micro_HC05/NB_micro_HC05.ino
+++ NB_micro_HC05/NB_micro_HC05.ino
@@ -6,24 +6,51 @@
 CRGB leds[NUM_LEDS];
 uint32_t matchIDs[] = {123, 456, 789, 111, 222, 333};
 size_t matchArraySize = sizeof(matchIDs) / sizeof(matchIDs[0]);
-uint32_t ID = 123;
+uint32_t ID = 111;
 uint32_t matchID = 0;
 unsigned long lastMatchTime = 0;
-const unsigned long matchTimeout = 100;
+const unsigned long matchTimeout = 320;
 
-void containsValue(const uint32_t array[], size_t size, uint32_t targetValue) {
+bool containsValue(const uint32_t array[], size_t size, uint32_t targetValue) {
+    for (size_t i = 0; i < size; ++i) {
+        if (array[i] == targetValue) {
+            return true;  // Value found
+        }
+    }
+    
+    return false;  // Value not found
+}
+
+void match(const uint32_t array[], size_t size, uint32_t targetValue) {
   for (size_t i = 0; i < size; i++) {
-    if (array[i] == targetValue) {
+    if ((array[i] == targetValue)) {
+
+      // Set the seed for the random number generators
+      randomSeed(ID);
+      uint8_t h1 = random(256);  // Hue for color 1
+      uint8_t s1 = 255;          // Saturation for color 1
+      uint8_t b1 = 255;          // Brightness for color 1
+
+      randomSeed(matchID);
+      uint8_t h2 = random(256);  // Hue for color 2
+      uint8_t s2 = 255;          // Saturation for color 2
+      uint8_t b2 = 255;          // Brightness for color 2
+
+      // Convert HSB to RGB
+      CRGB color1 = CHSV(h1, s1, b1);
+      CRGB color2 = CHSV(h2, s2, b2);
+      CRGB blendedColor = blend(color1, color2, 128);  // Blend colors
+
       // Turn on the LEDs
-      leds[0] = CRGB::Red;
-      leds[1] = CRGB::Red;
-      leds[2] = CRGB::Red;
-      leds[3] = CRGB::Red;
-      leds[4] = CRGB::Red;
-      leds[5] = CRGB::Red;
-      leds[6] = CRGB::Red;
-      leds[7] = CRGB::Red;
-      leds[8] = CRGB::Red;
+      leds[0] = color1;
+      leds[1] = color1;
+      leds[2] = color1;
+      leds[3] = blendedColor;
+      leds[4] = blendedColor;
+      leds[5] = blendedColor;
+      leds[6] = color2;
+      leds[7] = color2;
+      leds[8] = color2;
       return;
     }
   }
@@ -41,7 +68,7 @@ void loop() {
   char buffer[10];
   sprintf(buffer, "/%u", ID);
   Serial1.write(buffer);
-  delay(5);
+  delay(8);
 
   if (Serial1.available() >= 1) {
     char firstChar = Serial1.read();
@@ -52,6 +79,7 @@ void loop() {
 
       // Read the next sizeof(uint32_t) bytes into the appropriate variable
       char buffer[sizeof(uint32_t) * 2 + 1]; // Twice the size for hex digits plus null terminator
+      delay(12);
       Serial1.readBytes(buffer, sizeof(uint32_t) * 2);
       buffer[sizeof(uint32_t) * 2] = '\0'; // Null-terminate the string
 
@@ -74,7 +102,9 @@ void loop() {
   }
 
   Serial.println(millis() - lastMatchTime);
-  containsValue(matchIDs, matchArraySize, matchID);
+  if (containsValue(matchIDs, matchArraySize, ID)) {
+    match(matchIDs, matchArraySize, matchID);
+  }
 
   // No match found
   if ((millis() - lastMatchTime) > matchTimeout) {
